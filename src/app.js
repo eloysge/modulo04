4bfd1b52658955813bb65db906a6eb571f7f57e6
Eloy Gontijo Martins <eloy@sgeinformatica.com.br>
2019-08-07 10:01:58 -0300

    modulo04: final - inclusão logomarca

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './app.css';
 import TechList from './components/TechList';
+import logoMarca from './assets/logo SGE.jpeg';
 
 function App() {
-  return <TechList />;
+  return (
+    <>
+      <img src={logoMarca} width="100" />
+      <TechList />
+    </>
+  );
 }
 
 export default App;
